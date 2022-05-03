9af7d5a24ce13ff6ee588d824785b20d9c6ebfa1
Indra W <indra.roadota@gmail.com>
2022-05-03 23:44:38 +0700

    add stripe js payment

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -4,11 +4,21 @@
 // // import Cart from "./pages/Cart"
 
 import React from 'react'
+import Success from './Success'
+import Pay from './Pay'
+
 
 const App = () => {
   return (
     <Router>
-      
+      <Switch>
+        <Route path="/pay">
+          <Pay />
+        </Route>
+        <Route path="/success">
+          <Success/>
+        </Route>
+      </Switch>
     </Router>
   )
 }
